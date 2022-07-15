8002701c82cc48da312db19d18ed452053990622
Samvel <samvel.martirosyan@esterox.am>
2022-07-15 23:55:40 +0400

    qr generate

---
+++ app/Http/Controllers/QrController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Endroid\QrCode\Builder\Builder;
+use Endroid\QrCode\Color\Color;
+use Endroid\QrCode\Encoding\Encoding;
+use Endroid\QrCode\ErrorCorrectionLevel\ErrorCorrectionLevelHigh;
+use Endroid\QrCode\RoundBlockSizeMode\RoundBlockSizeModeMargin;
+use Endroid\QrCode\Writer\PngWriter;
+use Illuminate\Http\Request;
+
+class QrController extends Controller
+{
+    public function QrGenerator(Request $request)
+    {
+        $result = Builder::create()
+            ->writer(new PngWriter())
+            ->writerOptions([])
+            ->data($request->input('name'))
+            ->encoding(new Encoding('UTF-8'))
+            ->errorCorrectionLevel(new ErrorCorrectionLevelHigh())
+            ->size(300)
+            ->margin(10)
+            ->roundBlockSizeMode(new RoundBlockSizeModeMargin())
+            ->foregroundColor(new Color(65, 35, 100))
+            ->build();
+
+        $fp = fopen('../../resources/img', 'w');
+        fwrite($fp, $result->getString());
+        fclose($fp);
+    }
+}
